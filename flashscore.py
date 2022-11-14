f01417349257dead55cc421c4b15a2d9d09742cf
Damien Gourbeyre <Damien.Gourbeyre@grenoble-inp.org>
2022-11-14 13:03:12 +0100

    add stats & infos for match pages in db

--- flashscore.py
+++ flashscore.py
@@ -31,26 +31,39 @@ options.add_argument("--headless")
 driver = webdriver.Firefox(options=options)
 pages = []
 
-#choose pages
-pages.append("https://www.flashscore.fr/football/angleterre/premier-league/")
-pages.append("https://www.flashscore.fr/football/allemagne/bundesliga/")
-pages.append("https://www.flashscore.fr/football/france/ligue-1/")
-pages.append("https://www.flashscore.fr/football/espagne/laliga/")
-pages.append("https://www.flashscore.fr/football/italie/serie-a/")
+#choose leagues
+#pages.append("https://www.flashscore.fr/football/angleterre/premier-league/")
+#pages.append("https://www.flashscore.fr/football/allemagne/bundesliga/")
+#pages.append("https://www.flashscore.fr/football/france/ligue-1/")
+#pages.append("https://www.flashscore.fr/football/espagne/laliga/")
+#pages.append("https://www.flashscore.fr/football/italie/serie-a/")
+
+
+
+# récuperer la liste des matchs
 liste_url_matchs = []
 
+debug = False
 for page in pages:
+    liste_matchs_ligue = []
     browser = webdriver.Firefox(options=options)
     browser.get(page)
     sleep(1)
     tab=browser.find_elements_by_class_name('event__match--scheduled')
     for elem in tab:
-        liste_url_matchs.append(elem.get_attribute("id")[4:])#pour prendre en compte le _ ok
-
+        liste_matchs_ligue.append(elem.get_attribute("id")[4:])#pour prendre en compte le _ ok
+    if "bundesliga" in page:
+        liste_url_matchs += liste_matchs_ligue[0:9] #9 matchs par journée
+    else:
+        liste_url_matchs += liste_matchs_ligue[0:10] #10 matchs par journée
     browser.quit()
+
+liste_url_matchs=["tfzLo9sb"]
 print(len(liste_url_matchs), " matchs this gw")
 
+
 for k in range(len(liste_url_matchs)):
+    # pour chacun des matchs
     print("----------------------------------------------------------------")
     page = "https://www.flashscore.fr/match/"+liste_url_matchs[k]+"/#/resume-du-match"
     browser = webdriver.Firefox(options=options)
@@ -69,14 +82,13 @@ for k in range(len(liste_url_matchs)):
     away_team = browser.find_element_by_class_name("duelParticipant__away").text
     #fetch gamedate
     gameDate = browser.find_element_by_class_name("duelParticipant__startTime").text
+    gameHour = gameDate[-5:]
     gameDate = datetime.strptime(gameDate[0:6]+gameDate[8:10], '%d.%m.%y').strftime("%Y-%m-%d")
-    #gameDate = date.today().strftime("%Y-%m-%d")
     #fetch goals from rankings
     page_dom = "https://www.flashscore.fr/match/"+liste_url_matchs[k]+"/#/classement/table/overall"
     browser.close()
     browser = webdriver.Firefox(options=options)
     browser.get(page_dom)
-    #html = browser.page_source
     sleep(1)
 
     #fetch home & way team goals
@@ -119,7 +131,7 @@ for k in range(len(liste_url_matchs)):
         customAS = 0
         customAST = 0
     league = teamStats.loc[teamStats["Team"]==home_team]['Div'].values[0]
-    print(f"HS {customHS} AS {customAS} HS {customHST} AS {customAST} league {league} gd {gameDate}")
+    print(f"HS {customHS} AS {customAS} HST {customHST} AST {customAST} league {league} gd {gameDate}")
 
     topredict = pd.DataFrame(np.array([[cotes[0], cotes[1], cotes[1], customHS, customAS, customHST, customAST, cotes[3]]]),
                    columns=['B365H', 'B365D', 'B365A', 'CustomHS', 'CustomAS', 'CustomHST', 'CustomAST', 'CustomPSDG'])
@@ -135,22 +147,22 @@ for k in range(len(liste_url_matchs)):
     elif(league=="D1"):
         prono = d1.predict(topredict)
     
+    if not(debug):
+        #add game to matchs table
+        matchs.execute("""INSERT INTO MATCHS(id, date, HomeTeam, AwayTeam, ODD_HOME, ODD_DRAW, ODD_AWAY, OD_DRAW_OR_AWAY, PSHG, PSAG, CustomHS, CustomHST, CustomAS, CustomAST, gameHour) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""", (liste_url_matchs[k], gameDate, home_team, away_team, cotes[0], cotes[1], cotes[2], double_chance(cotes[1], cotes[2]), int(pshg), int(psag), round(customHS, 2), round(customHST, 2), round(customAS, 2), round(customAST, 2), gameHour))
 
-    #add game to matchs table
-    matchs.execute("""INSERT INTO MATCHS(id, date, HomeTeam, AwayTeam, ODD_HOME, ODD_DRAW, ODD_AWAY, OD_DRAW_OR_AWAY, PSHG, PSAG) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""", (liste_url_matchs[k], gameDate, home_team, away_team, cotes[0], cotes[1], cotes[2], double_chance(cotes[1], cotes[2]), int(pshg), int(psag)))
+        # get argmax, percentage
+        if (np.argmax(prono)==0):
+            confidence = round(prono[0][0]*100)
+            print(f"\t home team to win @{cotes[0]} {confidence}%")
+            matchs.execute("""INSERT INTO PRONOS(id, prono, confidence) VALUES(%s, %s, %s)""", (liste_url_matchs[k], 1, confidence))
 
-    # get argmax, percentage
-    if (np.argmax(prono)==0):
-        confidence = round(prono[0][0]*100)
-        print(f"\t home team to win @{cotes[0]} {confidence}%")
-        matchs.execute("""INSERT INTO PRONOS(id, prono, confidence) VALUES(%s, %s, %s)""", (liste_url_matchs[k], 1, confidence))
-
-    else:
-        confidence = round(prono[0][1]*100)
-        print(f"\t away team or draw @{double_chance(cotes[1], cotes[2])} {confidence}%")
-        matchs.execute("""INSERT INTO PRONOS(id, prono, confidence) VALUES(%s, %s, %s)""", (liste_url_matchs[k], -1, confidence))
-    
-    mydb.commit()
+        else:
+            confidence = round(prono[0][1]*100)
+            print(f"\t away team or draw @{double_chance(cotes[1], cotes[2])} {confidence}%")
+            matchs.execute("""INSERT INTO PRONOS(id, prono, confidence) VALUES(%s, %s, %s)""", (liste_url_matchs[k], -1, confidence))
+        
+        mydb.commit()
 
 print("----------------------------------------------------------------")
 
--- update_pronos.py
+++ update_pronos.py
@@ -32,7 +32,7 @@ rows = cursor.fetchall()
 options = FirefoxOptions()
 options.add_argument("--headless")
 
-
+debug = False
 for row in rows:	
 	adresse="https://www.flashscore.fr/match/"+row[0]+"/#/resume-du-match/statistiques-du-match/0"
 	r = requests.get(adresse)
@@ -50,8 +50,11 @@ for row in rows:
 				else:
 					resultat = -1
 				cursor.execute("""UPDATE MATCHS set FTR=%s, FTHG=%s, FTAG=%s WHERE id = (%s)""", (resultat, fthg, ftag, row[0]))
-				mydb.commit()
+				if not(debug):
+					mydb.commit()
 				cursor.execute("""SELECT PRONO from PRONOS where id = (%s)""", (row[0], ))
+				#ici on devra choisir les gains en fonction des trucs (suffit de prendre le cote AH2 et d'appliquer poru chaque type de pari si ca passe ou pas)
+				#condition sur le type de pari
 				if int(cursor.fetchall()[0][0]) == resultat:
 					if resultat == -1:
 						cursor.execute("""SELECT OD_DRAW_OR_AWAY from MATCHS where id = (%s)""", (row[0],))
@@ -62,7 +65,8 @@ for row in rows:
 				else:
 					gain = -1
 				cursor.execute("""UPDATE PRONOS set resultat = %s, gain = %s WHERE id = (%s)""", (resultat, gain, row[0]))
-				mydb.commit()
+				if not(debug):
+					mydb.commit()
 	
 	if score != ['', '']:
 		#updating csv now for teamStats
@@ -70,19 +74,85 @@ for row in rows:
 		browser.get(adresse)
 		element = WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".stat__homeValue")))
 		element = WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".stat__awayValue")))
+		element = WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".stat__categoryName")))
 		statsHome = browser.find_elements_by_class_name("stat__homeValue")
 		statsAway = browser.find_elements_by_class_name("stat__awayValue")
+		statsCateg = browser.find_elements_by_class_name("stat__categoryName")
 		teams = browser.find_elements_by_class_name("participant__participantName")
 		teams=[team.text for team in teams]
-
-		statsHome = [elem.text for elem in statsHome]
-		statsAway = [elem.text for elem in statsAway]
+		# HPOSS, HS, HST, HFC, HCORNERS, HOFFSIDES, HTOUCHES, HSAVES, HFOULS, HYELLOW, HRED, HPASSES, HTACKLES, HATKS 
+		# 	0	1	 2	  3		4			5			6		7		8		9		10		11		12		  13
+		homeStatsDB = [0 for _ in range(14)]
+		awayStatsDB = [0 for _ in range(14)]
+		for categ, statHome, statAway in zip(statsCateg, statsHome, statsAway):
+			# push them to the db
+			if categ.text.startswith("Possession"):
+				homeStatsDB[0]=int(statHome.text[:-1])
+				awayStatsDB[0]=int(statAway.text[:-1])
+			elif categ.text.startswith("Tirs au but"):
+				homeStatsDB[1]=int(statHome.text)
+				awayStatsDB[1]=int(statAway.text)
+			elif categ.text.startswith("Tirs cadr"):
+				homeStatsDB[2]=int(statHome.text)
+				awayStatsDB[2]=int(statAway.text)
+			elif categ.text.startswith("Coup francs"):
+				homeStatsDB[3]=int(statHome.text)
+				awayStatsDB[3]=int(statAway.text)
+			elif categ.text.startswith("Corners"):
+				homeStatsDB[4]=int(statHome.text)
+				awayStatsDB[4]=int(statAway.text)
+			elif categ.text.startswith("Hors-jeu"):
+				homeStatsDB[5]=int(statHome.text)
+				awayStatsDB[5]=int(statAway.text)
+			elif categ.text.startswith("Touche"):
+				homeStatsDB[6]=int(statHome.text)
+				awayStatsDB[6]=int(statAway.text)
+			elif categ.text.startswith("Sauvetages"):
+				homeStatsDB[7]=int(statHome.text)
+				awayStatsDB[7]=int(statAway.text)
+			elif categ.text.startswith("Fautes"):
+				homeStatsDB[8]=int(statHome.text)
+				awayStatsDB[8]=int(statAway.text)
+			elif categ.text.startswith("Cartons Jaunes"):
+				homeStatsDB[9]=int(statHome.text)
+				awayStatsDB[9]=int(statAway.text)
+			elif categ.text.startswith("Cartons Rouges"):
+				homeStatsDB[10]=int(statHome.text)
+				awayStatsDB[10]=int(statAway.text)
+			elif categ.text.startswith("Total Passes"):
+				homeStatsDB[11]=int(statHome.text)
+				awayStatsDB[11]=int(statAway.text)
+			elif categ.text.startswith("Tacles"):
+				homeStatsDB[12]=int(statHome.text)
+				awayStatsDB[12]=int(statAway.text)
+			elif categ.text.startswith("Attaques dange"):
+				homeStatsDB[13]=int(statHome.text)
+				awayStatsDB[13]=int(statAway.text)
+		
 		homeTeam = teams[0]
 		awayTeam = teams[2]
-		print(homeTeam, awayTeam, statsHome, statsAway)
+		print(homeTeam, awayTeam)
+		print(homeStatsDB, awayStatsDB)
+		
+		statsHome = [elem.text for elem in statsHome]
+		statsAway = [elem.text for elem in statsAway]
 		browser.close()
-		# update csv stats, useful for next bets
-		updateStatsCSV(homeTeam, awayTeam, int(statsHome[1]), int(statsHome[2]), int(statsAway[1]), int(statsAway[2]))
+		#update matchs with stats
+
+		# update csv stats, useful for next bets -> ok
+		if not(debug):
+			cursor.execute("""UPDATE MATCHS set HPOSS=%s, HSHOTS=%s, HST=%s, 
+												HFC=%s, HCORNERS=%s, HOFFSIDES=%s, 
+												HTOUCHES=%s, HSAVES=%s, HFOULS=%s, 
+												HYELLOW=%s, HRED=%s, HPASSES=%s, 
+												HTACKLES=%s, HATKS=%s,
+												APOSS=%s, ASHOTS=%s, AST=%s, 
+												AFC=%s, ACORNERS=%s, AOFFSIDES=%s, 
+												ATOUCHES=%s, ASAVES=%s, AFOULS=%s, 
+												AYELLOW=%s, ARED=%s, APASSES=%s, 
+												ATACKLES=%s, AATKS=%s  WHERE id = (%s)""", (*homeStatsDB, *awayStatsDB, row[0]))
+			mydb.commit()
+			updateStatsCSV(homeTeam, awayTeam, int(statsHome[1]), int(statsHome[2]), int(statsAway[1]), int(statsAway[2]))
 
 # save team stats
 teamStats.to_csv("teamStats.csv", index=False)
